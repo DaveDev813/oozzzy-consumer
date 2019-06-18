e19e1184b567e421b1894f2296cb5523f22455b4
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-06-18 18:31:21 +0800

    install nextjs and typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import App, { Container } from "next/app";
+import React from "react";
+
+export default class MainApp extends App {
+  constructor(props: any) {
+    super(props);
+  }
+
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
