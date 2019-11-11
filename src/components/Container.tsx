386a1cfdb2c6c56dafe701e784ac23d34cd5d499
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-11-11 21:02:52 +0800

    backup

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,7 @@
 const Container = props => {
-  return <div className={`container ${props.className}`}>{props.children}</div>;
+  return (
+    <div className={`container ${props.className} nigga`}>{props.children}</div>
+  );
 };
 
 export default Container;
